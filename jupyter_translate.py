180e42cf261f2479ac58684c8abb83bcc12aad8a
Andre L. Belem <andrebelem@id.uff.br>
2024-08-10 15:07:10 -0300

    Add files via upload

--- jupyter_translate.py
+++ jupyter_translate.py
@@ -1,6 +1,6 @@
 import json, fire, os, re
 from googletrans import Translator
-
+from tqdm import tqdm  # For progress bar
 
 def translate_markdown(text, dest_language='pt'):
     # Regex expressions
@@ -15,13 +15,13 @@ def translate_markdown(text, dest_language='pt'):
     IMG_PREFIX='!['
     HEADERS=['### ', '###', '## ', '##', '# ', '#'] # Should be from this order (bigger to smaller)
 
-     # Inner function to replace tags from text from a source list
+    # Inner function to replace tags from text from a source list
     def replace_from_list(tag, text, replacement_list):
         list_to_gen = lambda: [(x) for x in replacement_list]
         replacement_gen = list_to_gen()
         return re.sub(tag, lambda x: next(iter(replacement_gen)), text)
 
-    # Create an instance of Tranlator
+    # Create an instance of Translator
     translator = Translator()
 
     # Inner function for translation
@@ -64,41 +64,84 @@ def translate_markdown(text, dest_language='pt'):
 
     return translate(text)
 
-#export
+# Function to translate comments and formatted print statements in code cells
+def translate_code_comments_and_prints(code, dest_language='pt'):
+    translator = Translator()
+    lines = code.split('\n')
+    translated_lines = []
+    for line in lines:
+        if '#' in line:
+            # Split the line into code and comment parts
+            code_part, comment_part = line.split('#', 1)
+            # Translate the comment part
+            translated_comment = translator.translate(comment_part.strip(), dest=dest_language).text
+            # Reconstruct the line with translated comment
+            translated_lines.append(f"{code_part}# {translated_comment}")
+        elif 'print(f"' in line or "print(f'" in line:
+            # Handle formatted print statements
+            print_match = re.search(r'print\((f?)(["\'])(.*?)(\2)\)', line)
+            if print_match:
+                print_part = print_match.group(1)
+                text_part = print_match.group(3)
+                # Translate only the text within the formatted print statement
+                translated_text = translator.translate(text_part, dest=dest_language).text
+                # Reconstruct the line with translated text
+                translated_lines.append(f'print({print_part}"{translated_text}")')
+            else:
+                translated_lines.append(line)  # If it doesn't match, keep the line as is
+        else:
+            translated_lines.append(line)
+    return '\n'.join(translated_lines)
+
+# Export function with statistics and progress
 def jupyter_translate(fname, language='pt', rename_source_file=False, print_translation=False):
     """
-    TODO:
-    add dest_path: Destination folder in order to save the translated files.
+    Translates a Jupyter Notebook from English to the specified language.
     """
     with open(fname, 'r', encoding='utf-8') as file:
         data_translated = json.load(file)
 
-    skip_row=False
-    for i, cell in enumerate(data_translated['cells']):
+    total_cells = len(data_translated['cells'])
+    code_cells = sum(1 for cell in data_translated['cells'] if cell['cell_type'] == 'code')
+    markdown_cells = sum(1 for cell in data_translated['cells'] if cell['cell_type'] == 'markdown')
+
+    print(f"Total cells: {total_cells}")
+    print(f"Code cells: {code_cells}")
+    print(f"Markdown cells: {markdown_cells}")
+
+    skip_row = False
+    for i, cell in enumerate(tqdm(data_translated['cells'], desc="Translating cells", unit="cell")):
         for j, source in enumerate(cell['source']):
-            if cell['cell_type']=='markdown':
-                if source[:3]=='```':
-                    skip_row = not skip_row # Invert flag until I find next code block
+            if cell['cell_type'] == 'markdown':
+                if source[:3] == '```':
+                    skip_row = not skip_row  # Invert flag until the next code block
 
                 if not skip_row:
-                    if source not in ['```\n', '```', '\n'] and source[:4] != '<img':  # Don't translate cause
-                    # of: 1. ``` -> ëëë 2. '\n' disappeared 3. image's links damaged
+                    if source not in ['```\n', '```', '\n'] and source[:4] != '<img':  # Don't translate because of:
+                        # 1. ``` -> ëëë 2. '\n' disappeared 3. image links damaged
                         data_translated['cells'][i]['source'][j] = \
                             translate_markdown(source, dest_language=language)
+            elif cell['cell_type'] == 'code':
+                # Translate comments and formatted print statements within code cells
+                data_translated['cells'][i]['source'][j] = \
+                    translate_code_comments_and_prints(source, dest_language=language)
+
             if print_translation:
                 print(data_translated['cells'][i]['source'][j])
 
     if rename_source_file:
-        fname_bk = f"{'.'.join(fname.split('.')[:-1])}_bk.ipynb" # index.ipynb -> index_bk.ipynb
+        fname_bk = f"{'.'.join(fname.split('.')[:-1])}_bk.ipynb"  # index.ipynb -> index_bk.ipynb
 
         os.rename(fname, fname_bk)
         print(f'{fname} has been renamed as {fname_bk}')
 
-        open(fname,'w').write(json.dumps(data_translated))
+        with open(fname, 'w', encoding='utf-8') as f:
+            json.dump(data_translated, f, ensure_ascii=False, indent=2)
         print(f'The {language} translation has been saved as {fname}')
     else:
-        dest_fname = f"{'.'.join(fname.split('.')[:-1])}_{language}.ipynb" # any.name.ipynb -> any.name_pt.ipynb
-        open(dest_fname,'w').write(json.dumps(data_translated))
+        dest_fname = f"{'.'.join(fname.split('.')[:-1])}_{language}.ipynb"  # any.name.ipynb -> any.name_pt.ipynb
+        with open(dest_fname, 'w', encoding='utf-8') as f:
+            json.dump(data_translated, f, ensure_ascii=False, indent=2)
         print(f'The {language} translation has been saved as {dest_fname}')
 
 def markdown_translator(input_fpath, output_fpath, input_name_suffix=''):
@@ -114,3 +157,4 @@ def markdown_translator(input_fpath, output_fpath, input_name_suffix=''):
 
 if __name__ == '__main__':
     fire.Fire(jupyter_translate)
+
